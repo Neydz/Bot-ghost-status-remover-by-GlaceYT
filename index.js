b595310b985849350274e87e2112267d587f1eaf
Neydz <95901798+Neydz@users.noreply.github.com>
2024-11-29 01:45:59 +0200

    index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["Спостерігає за Темпестом 👀", "👀 Досі спостерігає за Темпестом"];
 const statusTypes = [ 'dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
